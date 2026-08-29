62f606add61892e4bd197f50833cc6aac3c918cf
Mikelolpo10 <xzvmike@gmail.com>
2026-08-29 15:23:43 +0700

    Improve hero section

--- Frontend/src/pages/homepage/Homepage.tsx
+++ Frontend/src/pages/homepage/Homepage.tsx
@@ -11,9 +11,27 @@ export default function Homepage() {
   return (
     <>
       <div className="mt-16 w-full flex flex-col bg-gray">
-        <section className="relative px-16 w-full flex min-w-0 min-h-0">
-          <h1 className="absolute top-16 left-[50%] text-primary text-4xl font-semibold translate-x-[-50%]">Made To Last, Made For You.</h1>
-          <img src={mainAds} alt="" fetchPriority="high" />
+        <section className="relative w-full flex min-w-0 min-h-0">
+          <img
+            src={mainAds}
+            alt="main_ads"
+            fetchPriority="high"
+            className="w-full"
+            height={800}
+            width={2000}
+          />
+          <div className="absolute bottom-0 left-0 pb-24 pl-16 text-white">
+            <h1 className="text-5xl font-semibold">
+              Made To Last, <br />
+              Made For You.
+            </h1>
+            <Link
+              to='categories'
+              className="mt-8 py-3 px-3 flex w-60 h-12 items-center justify-center rounded bg-primary text-lg font-medium shadow-2xl">
+              Shop Now
+            </Link>
+          </div>
+          {/* Tambah cta ke page categories */}
         </section>
 
         <TopSeller />
@@ -24,7 +42,7 @@ export default function Homepage() {
         {/* Product Categories */}
         <ProductCategories />
 
-        <section className="mt-20 mb-16 mx-16 flex flex-col items-center bg-gray-dark">
+        <section className="mt-20 mb-16 mx-16 flex flex-col items-center bg-gray-200">
           <h2 className="mt-10 mb-12 text-4xl font-bold">OUR MARKETPLACE!</h2>
           <div className="mb-16 px-60 w-full flex justify-between">
             <Link to='https://www.blibli.com/brand/polini' target="_blank" className="p-2 flex items-center h-24 w-44 rounded-xl transition-all hover:bg-white hover:scale-110">
