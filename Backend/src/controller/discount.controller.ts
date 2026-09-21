df34e5bcbb4da3a52275d7fe1d8eb7c51f1905a7
Mikelolpo10 <xzvmike@gmail.com>
2026-09-21 18:09:44 +0700

    Fix todays deal api

--- Backend/src/controller/discount.controller.ts
+++ Backend/src/controller/discount.controller.ts
@@ -61,21 +61,32 @@ export const getTodaysSale = async (req: Request, res: Response) => {
         products.slug,
         products.image_url,
         products.description,
-        products.gender,
+        products.department,
         products.material,
         products.status,
         products.category_id,
-        products.price,
+        first_variant.price,
         product_categories.name AS category_name,
         product_categories.slug AS category_slug
       FROM promotion_products
-      INNER JOIN products ON promotion_products.product_id = products.id
-      LEFT JOIN product_categories ON product_categories.id = products.category_id
-      INNER JOIN promotions ON promotions.id = promotion_products.promotion_id
+      INNER JOIN products 
+        ON promotion_products.product_id = products.id
+      LEFT JOIN product_categories 
+        ON product_categories.id = products.category_id
+      INNER JOIN promotions 
+        ON promotions.id = promotion_products.promotion_id
+      LEFT JOIN LATERAL (
+        SELECT price
+        FROM product_variants
+        WHERE product_variants.product_id = products.id
+        ORDER BY is_default DESC
+        LIMIT 1
+      ) AS first_variant ON true
       WHERE promotion_products.start_at <= NOW()
-      AND promotion_products.end_at >= NOW()
-      AND promotions.slug = 'todays-deals';
+        AND promotion_products.end_at >= NOW()
+        AND promotions.slug = 'todays-deals';
     `)
+
     return res.status(200).json(result.rows)
   } catch (err) {
     return res.status(500).json({ message: 'Internal server error' })
